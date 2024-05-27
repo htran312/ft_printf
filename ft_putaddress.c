8a1bed99e928152671d7ea2b6dfe2c7991fd4847
Huong Tran Thi Thu <htran-th@c2r1p8.hive.fi>
2024-05-27 20:25:52 +0300

    change printaddress

--- ft_putaddress.c
+++ ft_putaddress.c
@@ -6,27 +6,35 @@
 /*   By: htran-th <htran-th@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/23 22:41:31 by htran-th          #+#    #+#             */
-/*   Updated: 2024/05/24 18:39:37 by htran-th         ###   ########.fr       */
+/*   Updated: 2024/05/27 20:21:36 by htran-th         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int ft_putaddress(unsigned long long int n)
+static int ft_putaddress_helper(unsigned long long int n)
 {
     int count;
     char *symbol;
-
+    
     count = 0;
     symbol = "0123456789abcdef";
-    if (n == 0)
-		count += write(1, "0", 1);
-    else if (n < 16)
+    if (n < 16)
         count += ft_putchar(symbol[n]);
     else
     {
-        count += ft_putaddress(n / 16);
+        count += ft_putaddress_helper(n / 16);
         count += ft_putchar(symbol[n % 16]);
     }
     return (count);
+}
+int ft_putaddress(unsigned long long int n)
+{
+    int count;
+    
+    count = 0;
+    count = ft_putaddress_helper(n);
+    if (count == -1)
+        return (-1);
+    return (count + 2);
 }
